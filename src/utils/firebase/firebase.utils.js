2465672c597e4fa783296b1fadf2be1d3aa8dcb3
Mike Goforth <mike.goforth@outlook.com>
2022-05-31 00:20:32 -0500

    feature: added sign in component as well as firebase utils and brought in firebase libraries

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,54 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyABsOIgbhoLLM19wmUJkr827yAp4hmEah4",
+  authDomain: "space-bar-clothing-db.firebaseapp.com",
+  projectId: "space-bar-clothing-db",
+  storageBucket: "space-bar-clothing-db.appspot.com",
+  messagingSenderId: "780154827877",
+  appId: "1:780154827877:web:abd1930e053193ffda028c",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    const userSnapshot = await getDoc(userDocRef);
+
+    if(!userSnapshot.exists()) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt,
+            });
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    return userDocRef;
+}
