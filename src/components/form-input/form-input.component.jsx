0954fc324bb0eea117a7a3996471c7804193c282
Mike Goforth <mike.goforth@outlook.com>
2022-06-05 02:43:17 -0500

    feature: style the form-input component

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,10 +1,12 @@
-import { FormInputLabel, Input, Group } from './form-input.styles';
+import { FormInputLabel, Input, Group } from "./form-input.styles";
 
 const FormInput = ({ label, ...otherProps }) => {
+  // console.log(otherProps.value);
   return (
     <Group>
       <Input {...otherProps} />
       {label && (
+        // TODO: Fix shrink issue on otherProps.value
         <FormInputLabel shrink={otherProps.value}>
           {label}
         </FormInputLabel>
