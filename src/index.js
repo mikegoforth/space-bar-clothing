956e11436c8dd97b1818321123684379c2fbd2bd
Mike Goforth <mike.goforth@outlook.com>
2022-06-06 00:13:14 -0500

    feature: add reselect, implement redux on cart, and minor optimizations

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import App from "./App";
-import { CartProvider } from "./contexts/cart.context";
 import { store } from "./store/store";
 
 import "./index.scss";
@@ -15,10 +14,8 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CartProvider>
-          <App />
-        </CartProvider>
+        <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
