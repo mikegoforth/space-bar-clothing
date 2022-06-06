ad4f56cb595e532c199b41f42fbcc9d68e66c79a
Mike Goforth <mike.goforth@outlook.com>
2022-06-06 00:31:22 -0500

    feature: add redux-persist and re-implement logger for dev mode

--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,28 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import { rootReducer } from './root-reducer';
+import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
 
-const loggerMiddleware = (store) => (next) => (action) => {
-    if (!action.type) {
-        return next(action);
-    }
+import { rootReducer } from "./root-reducer";
 
-    console.log('type: ', action.type);
-    console.log('payload: ', action.payload);
-    console.log('currentState ', store.getState());
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
 
-    next(action);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-    console.log('next state: ', store.getState());
-}
-
-const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+  Boolean
+);
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
