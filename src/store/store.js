1bb1526d035c49e69f86a47b14486b66e6690f4c
Mike Goforth <mike.goforth@outlook.com>
2022-06-06 21:43:48 -0500

    chore: consider configureStore in favor of deprecated createStore

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
@@ -11,24 +12,10 @@ const persistConfig = {
   blacklist: ["user"],
 };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
-  Boolean
-);
-
-const composeEnhancer =
-  (process.env.NODE_ENV !== "production" &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
-
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
-
-export const store = createStore(
-  persistedReducer,
-  undefined,
-  composedEnhancers
-);
+export const store = configureStore({
+  reducer: persistReducer(persistConfig, rootReducer),
+  middleware: [logger],
+  devTools: true
+});
 
 export const persistor = persistStore(store);
