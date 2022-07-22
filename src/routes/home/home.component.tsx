0b09332eb43118b2ef252d0014f955798447f3e7
Mike Goforth <mike.goforth@outlook.com>
2022-07-22 15:46:10 -0500

    chore: type out home, shop, and checkout routes

--- src/routes/home/home.component.tsx
+++ src/routes/home/home.component.tsx
@@ -1,9 +1,8 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 
-import Directory from "../../components/directory/directory.component";
+import Directory from '../../components/directory/directory.component';
 
 const Home = () => {
-
   return (
     <div>
       <Directory />
